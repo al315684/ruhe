7b862fcdbd919c7c05eea70da9e8ad986195063b
Gema <al315684@uji.es>
2019-04-29 18:20:01 +0200

    adds some configurations

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+    <v-toolbar flat app color="#17A2B8" dark style="color: #DDF0F3">
+            <v-toolbar-side-icon @click="drawer = !drawer" style="color: #DDF0F3"></v-toolbar-side-icon>
+            <v-toolbar-title>
+                RUHE
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat style="color: #DDF0F3">Perfil</v-btn>
+        </v-toolbar>
+        <v-navigation-drawer v-model="drawer" app class="primary">
+            <p>Holi</p>
+        </v-navigation-drawer>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
